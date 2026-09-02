1b322f9dac7f3d61bfbd9f8514ac4cfe07e29ff1
seanperdomo <sean.perdomo@udea.edu.co>
2026-09-02 14:59:18 -0500

    Modifico el pipeline para que solo tome radios y masas

--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,3 @@
-QUERY="SELECT+pl_name,pl_rade,pl_bmasse+FROM+ps"
+QUERY="SELECT+pl_rade,pl_bmasse+FROM+ps"
 URL="https://exoplanetarchive.ipac.caltech.edu/TAP/sync?format=csv&query=${QUERY}"
 wget -q -O masasyradios.csv "$URL"
